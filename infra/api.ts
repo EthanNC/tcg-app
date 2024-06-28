34da9f2f9b37dc81afdff139047c1404d3c2a004
EthanNC <ethan.n.cumming@gmail.com>
2024-06-28 10:33:09 -0400

    api folder structure

--- infra/api.ts
+++ infra/api.ts
@@ -3,5 +3,5 @@ import { project } from "./database";
 export const api = new sst.aws.Function("Api", {
   url: true,
   link: [project],
-  handler: "./packages/functions/src/api.handler",
+  handler: "./packages/functions/src/api/index.handler",
 });
