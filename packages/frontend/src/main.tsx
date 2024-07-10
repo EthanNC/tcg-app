55f4cc27687afcc71edf486e1588932ac975b1c5
EthanNC <ethan.n.cumming@gmail.com>
2024-07-10 13:27:43 -0400

    frontend: add zod and react query devtools

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
@@ -25,6 +25,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </React.StrictMode>
 );
--- packages/frontend/src/routes/index.tsx
+++ packages/frontend/src/routes/index.tsx
@@ -1,8 +1,13 @@
 import { CommandMenu } from "@/components/CommandMenu";
 import { createFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
 
+const CardSearchSchema = z.object({
+  name: z.string().optional(),
+});
 export const Route = createFileRoute("/")({
   component: Index,
+  validateSearch: (search) => CardSearchSchema.parse(search),
 });
 
 export default function Index() {
