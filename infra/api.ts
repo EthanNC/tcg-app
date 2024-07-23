0dcb00ac0d0d19817d5eaa4d8eafc0f4075722d2
EthanNC <ethan.n.cumming@gmail.com>
2024-07-23 13:04:06 -0400

    add hashing to dependency to lambda function installer

--- infra/api.ts
+++ infra/api.ts
@@ -3,5 +3,8 @@ import { project } from "./database";
 export const api = new sst.aws.Function("Api", {
   url: true,
   link: [project],
+  nodejs: {
+    install: ["@node-rs/argon2"],
+  },
   handler: "./packages/functions/src/api/index.handler",
 });
